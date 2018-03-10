a81dc06c404b61657539202719fa4faa800ccb4a
agperkins <alexandrasaff@gmail.com>
2018-03-10 17:03:25 -0600

    Add wrapper component

---
+++ components/wrapper.js
@@ -0,0 +1,19 @@
+var navigation = require('../components/navigation')
+var html = require('choo/html')
+
+var title = 'Han'
+
+module.exports = wrapper
+
+function wrapper (view) {
+  return function (state, emit) {
+    if (state.title !== title) emit(state.events.DOMTITLECHANGE, title)
+    state.page = state.page || { }
+    return html`
+      <body class="code lh-copy">
+        ${navigation()}
+        ${view(state, emit)}
+      </body>
+    `
+  }
+}
