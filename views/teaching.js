64169d7e409a475445077c86233c881942b33324
agperkins <alexandrasaff@gmail.com>
2018-03-11 18:06:37 -0500

    Add copy teaching

--- views/teaching.js
+++ views/teaching.js
@@ -9,8 +9,19 @@ function view (state, emit) {
   if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
   return html`
     <main class="sans-serif pa3">
-      <h1>Teaching, Losers</h1>
-      <p>I'm 33 and learning to code</p>
+      <h1>Teaching</h1>
+      <p>Teaching is an opportunity to challenge, reconfigure, and affirm peoples' understanding of the world.</p>
+      <p> My courses range from Spanish 101 to Advanced Seminars in Performance and Identity.</p>
+      <p> Check it out:</p>
+      <ul>
+        <li>Bodies in text: The writing of the self from Autobiography to Facebook</li>
+        <li>Narrativas del Boom y Post-Boom Latinoamericano</li>
+        <li>Latino/a Literature and Culture</li>
+        <li>Survey of Spanish American Literature</li>
+        <li>Survey of Peninsular Literature</li>
+        <li>Spanish American Civilization</li>
+        <li>Spanish Conversation</li>
+        <li>Advanced Grammar and Composition</li>
     </main>
   `
 }
