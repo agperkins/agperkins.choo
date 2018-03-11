e433bf525951d3fcdc0f5690fd67b62b8ba515d3
agperkins <alexandrasaff@gmail.com>
2018-03-11 18:05:17 -0500

    Add link and copy

--- views/contactme.js
+++ views/contactme.js
@@ -9,8 +9,9 @@ function contactme (state, emit) {
   if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
   return html`
     <main class="sans-serif pa3">
-      <h1>Contact Me, Losers</h1>
-      <p>I'm 33 and learning to code</p>
+      <h1>Where to Find Me</h1>
+      <p>You can email me at aperkins@ua.edu</p>
+      <p><p>Check my academia.edu <a href="http://crimson.academia.edu/AlexandraGonzenbachPerkins">page</a> for copies of my publications.</p>
     </main>
   `
 }
