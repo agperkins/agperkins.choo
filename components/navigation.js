147813508c7fc2e2cd229580876cb082b568231f
agperkins <alexandrasaff@gmail.com>
2018-03-11 18:04:26 -0500

    Delete example page

--- components/navigation.js
+++ components/navigation.js
@@ -4,15 +4,14 @@ module.exports = navigation
 
 function navigation () {
   return html`
-    <nav>
+    <nav class="pa3">
         <a class="navlink" href="/aboutme/">About Me</a> |
         <a class="navlink" href="/research/">Research</a> |
         <a class="navlink" href="/publications/">Publications</a> |
         <a class="navlink" href="/teaching/">Teaching</a> |
         <a class="navlink" href="/gallery/">Gallery</a> |
         <a class="navlink" href="/contactme/">Contact Me</a> |
-        <a class="navlink" href="/cv/">CV</a> |
-        <a class="navlink" href="/example/">Example</a>
+        <a class="navlink" href="/cv/">CV</a>
       </nav>
   `
 }
