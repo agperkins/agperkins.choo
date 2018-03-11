6777b08a613064f9dde180493f8415317a384cd2
agperkins <alexandrasaff@gmail.com>
2018-03-11 18:06:04 -0500

    Add images

--- views/gallery.js
+++ views/gallery.js
@@ -9,8 +9,17 @@ function view (state, emit) {
   if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
   return html`
     <main class="sans-serif pa3">
-      <h1>Gallery, Losers</h1>
-      <p>I'm 33 and learning to code</p>
+      <h1>Gallery</h1>
+      <p>Research requires participation.</p>
+      <p>From Lima to Chiapas, Miami to North Carolina, here are a few images of my participatory research</p>
+      <img src="/assets/alicasalit.jpg" alt="Casa de Literatura">
+      <img src="/assets/alidigitalgraffiti.jpg" alt="Digital Graffiti 2017, Alys Beach, FL">
+      <img src="/assets/alireception.jpg" alt="Reception at UM">
+      <img src="/assets/alistairs.jpg" alt="Poetry">
+      <img src="/assets/ChiapasPerformance.jpg" alt="Performance in Chiapas">
+      <img src="/assets/GalleryIsmael.jpg" alt="With Ismael Ogando">
+      <img src="/assets/GalleryJosefina.jpg" alt="With Josefina Baez">
+      <img src="/assets/LatinoGrantPhoto.jpg" alt="Grant Winners">
     </main>
   `
 }
