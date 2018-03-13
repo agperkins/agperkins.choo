e036fcc5bdb168b040df6a5f5d2c9d6cf7cef0e3
agperkins <alexandrasaff@gmail.com>
2018-03-13 17:33:06 -0500

    Obfuscate email

--- views/contactme.js
+++ views/contactme.js
@@ -1,4 +1,6 @@
 var html = require('choo/html')
+var obfuscate = require('obfuscate')
+
 var wrapper = require('../components/wrapper')
 
 module.exports = wrapper(contactme)
@@ -10,7 +12,10 @@ function contactme (state, emit) {
   return html`
     <main class="sans-serif pa3">
       <h1>Where to Find Me</h1>
-      <p>You can email me at aperkins@ua.edu</p>
+      <p>You can email me at ${obfuscate({
+    email: 'aperkins@ua.edu',
+    subject: 'Love your website!'
+  })}</p>
       <p><p>Check my academia.edu <a href="http://crimson.academia.edu/AlexandraGonzenbachPerkins">page</a> for copies of my publications and syllabi.</p>
     </main>
   `
