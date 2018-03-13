fd39227c0e2fb0c2aeeff7d55f261c14e5f1f825
agperkins <alexandrasaff@gmail.com>
2018-03-13 17:01:49 -0500

    Update nav main about me

--- components/navigation.js
+++ components/navigation.js
@@ -5,7 +5,7 @@ module.exports = navigation
 function navigation () {
   return html`
     <nav class="pa3">
-        <a class="navlink" href="/aboutme/">About Me</a> |
+        <a class="navlink" href="/">About Me</a> |
         <a class="navlink" href="/research/">Research</a> |
         <a class="navlink" href="/publications/">Publications</a> |
         <a class="navlink" href="/teaching/">Teaching</a> |
